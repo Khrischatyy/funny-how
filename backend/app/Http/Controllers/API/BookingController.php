7cd1a6080d61c8e3e06146500a7a4008998d741d
tony <khrischatyy@gmail.com>
2024-05-11 04:45:51 -0700

    dev/ made booking

--- backend/app/Http/Controllers/API/BookingController.php
+++ backend/app/Http/Controllers/API/BookingController.php
@@ -2,130 +2,27 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Exceptions\BookingException;
 use App\Http\Controllers\BaseController;
 use App\Http\Requests\BookingRequest;
 use App\Http\Requests\ReservationRequest;
-use App\Models\Address;
-use App\Models\Booking;
-use App\Models\OperatingHour;
-use App\Models\StudioClosure;
-use App\Models\User;
 use App\Services\BookingService;
-use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class BookingController extends BaseController
 {
-    public function __construct(public BookingService $bookingService)
+    public function __construct(private BookingService $bookingService)
     {}
 
-    public function getAllReservations(ReservationRequest $reservationRequest)
+    public function getAllReservations(ReservationRequest $reservationRequest): JsonResponse
     {
-        $date = $reservationRequest->input('date');
-        $addressId = $reservationRequest->input('address_id');
+        $data = $this->bookingService->getAllReservations($reservationRequest);
 
-        // Get all bookings for the specified date
-        $bookings = Booking::where('address_id', $addressId)
-            ->where('date', $date)
-            ->orderBy('start_time', 'asc')
-            ->get();
-
-        // Assume the studio's working hours are from 08:00 to 22:00
-        $openTime = Carbon::parse($date . ' 08:00');
-        $closeTime = Carbon::parse($date . ' 22:00');
-
-        $availableSlots = [];
-        $previousEndTime = $openTime;
-
-        // Find all free slots
-        foreach ($bookings as $booking) {
-            $bookingStart = Carbon::parse($date . ' ' . $booking->start_time);
-            $bookingEnd = Carbon::parse($date . ' ' . $booking->end_time);
-
-            // If there is free time between the end of the previous booking and the start of the new one
-            if ($previousEndTime->lt($bookingStart)) {
-                $availableSlots[] = [
-                    'start_time' => $previousEndTime->format('H:i'),
-                    'end_time' => $bookingStart->format('H:i')
-                ];
-            }
-
-            // Update the end time of the previous booking
-            $previousEndTime = $bookingEnd;
-        }
-
-        // Add the last free slot at the end of the day, if there is any
-        if ($previousEndTime->lt($closeTime)) {
-            $availableSlots[] = [
-                'start_time' => $previousEndTime->format('H:i'),
-                'end_time' => $closeTime->format('H:i')
-            ];
-        }
-
-        return $this->sendResponse([
-            'date' => $date,
-            'address_id' => $addressId,
-            'available_slots' => $availableSlots
-        ], 'Available slots received');
+        return $this->sendResponse($data, 'Available slots received');
     }
 
-    public function bookStudio(BookingRequest $bookingRequest)
+    public function bookStudio(BookingRequest $bookingRequest): JsonResponse
     {
-        $addressId = $bookingRequest->input('address_id');
-        $bookingDate = Carbon::parse($bookingRequest->input('date'));
-        $startTime = Carbon::parse($bookingRequest->input('date') . ' ' . $bookingRequest->input('start_time'));
-        $endTime = Carbon::parse($bookingRequest->input('date') . ' ' . $bookingRequest->input('end_time'));
-        $userWhoBooks = User::where('id', (int) $bookingRequest->input('user_id'))->first();
-
-        // Проверка, не закрыта ли студия в выбранный день
-        if (StudioClosure::where('address_id', $addressId)
-            ->where('closure_date', $bookingDate->toDateString())
-            ->exists()) {
-            throw new BookingException('Studio is closed on this date', 422);
-        }
-
-        // Проверка операционных часов студии
-        $dayOfWeek = $bookingDate->dayOfWeek;
-        $operatingHours = OperatingHour::where('address_id', $addressId)
-            ->where('day_of_week', $dayOfWeek)
-            ->first();
-
-        if (!$operatingHours || $operatingHours->is_closed) {
-            throw new BookingException('Studio is closed on this date', 422);
-        }
-
-        // Проверка, входит ли время бронирования в рабочее время
-        $openingTime = Carbon::parse($bookingDate->format('Y-m-d') . ' ' . $operatingHours->open_time);
-        $closingTime = Carbon::parse($bookingDate->format('Y-m-d') . ' ' . $operatingHours->close_time);
-        if ($startTime->lt($openingTime) || $endTime->gt($closingTime) || $endTime->lt($startTime)) {
-            throw new BookingException('Booking times are outside of business hours.', 422);
-        }
-
-        // Проверяем, нет ли пересечения с другими бронированиями
-        if (Booking::where('address_id', $addressId)
-            ->where(function ($query) use ($startTime, $endTime) {
-                $query->where(function ($q) use ($startTime, $endTime) {
-                    // Проверяем, что новое время начала строго раньше времени окончания других бронирований
-                    // и что новое время окончания строго до начала других бронирований
-                    $q->where('start_time', '<', $endTime)
-                        ->where('end_time', '>', $startTime);
-                });
-            })->exists()) {
-            throw new BookingException('Studio is already booked for the requested time slot', 400);
-        }
-
-        // Создание бронирования
-        $booking = Booking::create([
-            'address_id' => $addressId,
-            'start_time' => $startTime,
-            'end_time' => $endTime,
-            'user_id' => $userWhoBooks->id,
-            'total_cost' => 30,
-            'date' => $bookingDate->format('Y-m-d'),
-        ]);
-        $booking->save();
+        $booking = $this->bookingService->bookStudio($bookingRequest);
 
         return $this->sendResponse($booking, 'Studio booked successfully');
     }
--- backend/app/Services/BookingService.php
+++ backend/app/Services/BookingService.php
@@ -2,52 +2,115 @@
 
 namespace App\Services;
 
-use App\Repositories\BookingRepository;
+use App\Exceptions\BookingException;
+use App\Http\Requests\BookingRequest;
+use App\Http\Requests\ReservationRequest;
+use App\Models\Booking;
+use App\Models\OperatingHour;
+use App\Models\StudioClosure;
+use App\Models\User;
 use Carbon\Carbon;
-use Doctrine\Inflector\Rules\Word;
 
 class BookingService
 {
-    public function __construct(public BookingRepository $bookingRepository)
-    {}
-
-    public function getFreeSlots(int $addressId,Carbon $day): array
+    public function getAllReservations(ReservationRequest $request): array
     {
-        $addressWithBookings = $this->bookingRepository->getBookingByAddressId($addressId)->first();
+        $date = $request->input('date');
+        $addressId = $request->input('address_id');
+        $bookings = Booking::where('address_id', $addressId)
+            ->where('date', $date)
+            ->orderBy('start_time', 'asc')
+            ->get();
 
-        $works_since = $day->copy()->setTimeFrom($addressWithBookings->works_since);
-        $works_till = $day->copy()->setTimeFrom($addressWithBookings->works_till);
+        $dayOfWeek = Carbon::parse($date)->dayOfWeek;
+        $operatingHours = $this->getOperatingHours($addressId, $dayOfWeek);
 
-        $reservedTime = $addressWithBookings->bookings->map(function ($booking) {
-            return $this->getWorkingTime(Carbon::create($booking->from), Carbon::create($booking->to));
-        })->flatten(1)->toArray();
+        $openTime = Carbon::parse($date . ' ' . $operatingHours->open_time);
+        $closeTime = Carbon::parse($date . ' ' . $operatingHours->close_time);
 
-        $workingTimes = $this->getWorkingTime($works_since, $works_till);
 
-        $freeSlots = [];
-
-        foreach ($workingTimes as $workingTime) {
-            if (!in_array($workingTime, $reservedTime)) {
-                $freeSlots[] = $workingTime;
-            }
-        }
-
-        return $freeSlots;
+        return $this->calculateAvailableSlots($bookings, $openTime, $closeTime);
     }
 
-    private function getWorkingTime($since, $till): array
+    private function calculateAvailableSlots($bookings, $openTime, $closeTime): array
     {
-        $countAvailableHours = $since->diffInHours($till);
+        $availableSlots = [];
+        $previousEndTime = $openTime;
 
-        $workingTime = [[$since, $since->copy()->addHour()]];
+        foreach ($bookings as $booking) {
+            $bookingStart = Carbon::parse($booking->start_time);
+            $bookingEnd = Carbon::parse($booking->end_time);
 
-        for ($i = 0; $i < $countAvailableHours - 1; $i++) {
+            if ($previousEndTime->lt($bookingStart)) {
+                $availableSlots[] = [
+                    'start_time' => $previousEndTime->format('H:i'),
+                    'end_time' => $bookingStart->format('H:i')
+                ];
+            }
+            $previousEndTime = $bookingEnd;
+        }
 
-            $prevTime = $workingTime[$i];
+        if ($previousEndTime->lt($closeTime)) {
+            $availableSlots[] = [
+                'start_time' => $previousEndTime->format('H:i'),
+                'end_time' => $closeTime->format('H:i')
+            ];
+        }
 
-            $workingTime[] = [$prevTime[0]->copy()->addHour(), $prevTime[1]->copy()->addHour()];
-        };
+        return $availableSlots;
+    }
 
-        return $workingTime;
+    public function bookStudio(BookingRequest $request): Booking
+    {
+        $addressId = $request->input('address_id');
+        $bookingDate = Carbon::parse($request->input('date'));
+        $startTime = Carbon::parse($request->input('start_time'));
+        $endTime = Carbon::parse($request->input('end_time'));
+        $userWhoBooks = User::find((int) $request->input('user_id'));
+
+        $this->validateStudioAvailability($addressId, $bookingDate, $startTime, $endTime);
+
+        return Booking::create([
+            'address_id' => $addressId,
+            'start_time' => $startTime,
+            'end_time' => $endTime,
+            'user_id' => $userWhoBooks->id,
+            'total_cost' => 30, // This should be calculated based on business logic
+            'date' => $bookingDate->format('Y-m-d'),
+        ]);
+    }
+
+    private function validateStudioAvailability($addressId, $bookingDate, $startTime, $endTime)
+    {
+        if (StudioClosure::where('address_id', $addressId)->where('closure_date', $bookingDate->toDateString())->exists()) {
+            throw new BookingException('Studio is closed on this date', 422);
+        }
+
+        $operatingHours = $this->getOperatingHours($addressId, $bookingDate->dayOfWeek);
+
+        if (!$operatingHours || $operatingHours->is_closed) {
+            throw new BookingException('Booking times are outside of business hours.', 422);
+        }
+
+        if ($startTime->lt($operatingHours->open_time) || $endTime->gt($operatingHours->close_time)) {
+            throw new BookingException("Booking times are outside of business hours. Studio opens at {$operatingHours->open_time} and closes at {$operatingHours->close_time}", 422);
+        }
+
+        if ($this->isTimeSlotTaken($addressId, $startTime, $endTime)) {
+            throw new BookingException('Studio is already booked for the requested time slot', 400);
+        }
+    }
+
+    private function isTimeSlotTaken($addressId, $startTime, $endTime): bool
+    {
+        return Booking::where('address_id', $addressId)
+            ->where('start_time', '<', $endTime)
+            ->where('end_time', '>', $startTime)
+            ->exists();
+    }
+
+    private function getOperatingHours($addressId, $dayOfWeek)
+    {
+        return OperatingHour::where('address_id', $addressId)->where('day_of_week', $dayOfWeek)->first();
     }
 }
