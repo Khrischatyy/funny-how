c6c8077cdd74f21f91010feb32a580a05cffc45c
tony <khrischatyy@gmail.com>
2024-05-18 04:21:38 -0700

    dev/ made get company by slug logo

--- backend/app/Models/Badge.php
+++ backend/app/Models/Badge.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Badge extends Model
 {
@@ -12,5 +13,10 @@ class Badge extends Model
     public function addresses()
     {
         return $this->belongsToMany(Address::class, 'address_badge');
+    }
+
+    public function getImageUrlAttribute()
+    {
+        return Storage::disk('s3')->url($this->image);
     }
 }
--- backend/app/Repositories/CompanyRepository.php
+++ backend/app/Repositories/CompanyRepository.php
@@ -7,23 +7,51 @@ use App\Models\City;
 use App\Models\Company;
 use App\Repositories\Interfaces\CompanyRepositoryInterface;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class CompanyRepository implements CompanyRepositoryInterface
 {
+    /**
+     * Get companies by city ID with their addresses and badges.
+     *
+     * @param int $cityId
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
     public function getCompaniesByCityId(int $cityId)
     {
-        return City::whereId($cityId)->with('companies.addresses.badges')->paginate();
+        $city = City::whereId($cityId)
+            ->with(['companies.addresses.badges' => function ($query) {
+                $query->select(['badges.*']);
+            }])
+            ->paginate();
+
+        // Map badges to include the S3 URLs and hide the pivot attribute
+        $city->companies->each(function ($company) {
+            $company->addresses->each(function ($address) {
+                $address->badges->each(function ($badge) {
+                    $badge->image_url = $badge->getImageUrlAttribute();
+                    $badge->makeHidden('pivot');
+                });
+            });
+        });
+
+        return $city;
     }
 
     public function getCompanyBySlug(string $slug)
     {
         $company = Company::where('slug', $slug)
-            ->with(['addresses' => function($q) {
-                $q->with('badges');
+            ->with(['addresses.badges' => function ($query) {
+                $query->select(['badges.*']);
             }])
             ->firstOrFail();
 
-        $company->logo_url = Storage::disk('s3')->url($company->logo);
+
+        $company->addresses->each(function ($address) {
+            $address->badges->each(function ($badge) {
+                $badge->image_url = $badge->getImageUrlAttribute();
+            });
+        });
 
         return $company;
     }
