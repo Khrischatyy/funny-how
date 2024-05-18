7178d21ec98928c90fd588e4a12816ddb7e17728
tony <khrischatyy@gmail.com>
2024-05-18 03:32:47 -0700

    dev/ made logo in company

--- backend/app/Repositories/CompanyRepository.php
+++ backend/app/Repositories/CompanyRepository.php
@@ -6,6 +6,7 @@ use App\Exceptions\OperatingHourException;
 use App\Models\City;
 use App\Models\Company;
 use App\Repositories\Interfaces\CompanyRepositoryInterface;
+use Illuminate\Support\Facades\Storage;
 
 class CompanyRepository implements CompanyRepositoryInterface
 {
@@ -16,8 +17,14 @@ class CompanyRepository implements CompanyRepositoryInterface
 
     public function getCompanyBySlug(string $slug)
     {
-        return Company::where('slug', $slug)->with(['addresses' => function($q) {
-            $q->with('badges');
-        }])->firstOrFail();
+        $company = Company::where('slug', $slug)
+            ->with(['addresses' => function($q) {
+                $q->with('badges');
+            }])
+            ->firstOrFail();
+
+        $company->logo_url = Storage::disk('s3')->url($company->logo);
+
+        return $company;
     }
 }
