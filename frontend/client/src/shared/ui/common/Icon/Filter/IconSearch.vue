e88d72be0b95c3696b7cf680b1d67b95b25af562
tony <khrischatyy@gmail.com>
2024-06-09 19:41:07 -0700

    dev/ made icons

---
+++ frontend/client/src/shared/ui/common/Icon/Filter/IconSearch.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg width="22" height="23" viewBox="0 0 22 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="10.0832" cy="10.5827" r="6.41667" stroke="white"/>
+    <path d="M18.3335 18.834L15.5835 16.084" stroke="white" stroke-linecap="round"/>
+  </svg>
+</template>
+<script setup lang="ts">
+</script>
