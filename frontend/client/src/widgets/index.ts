873feb335dad86981daedce2b9146ec6f4fdf220
beilec <rushadaev@gmail.com>
2024-06-14 03:52:50 -0700

    front/add_studio

--- frontend/client/src/widgets/index.ts
+++ frontend/client/src/widgets/index.ts
@@ -1,2 +1,2 @@
 export {default as TimeSelect} from './TimeSelect.vue'
-export {default as HoursChoose} from './HoursChoose.vue'
+export {default as HoursChoose} from './HoursChoose/ui/HoursChoose.vue'
