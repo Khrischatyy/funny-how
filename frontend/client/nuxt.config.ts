fa0bce00baec572a73e577a41ed4ce7413661fab
tony <khrischatyy@gmail.com>
2024-06-15 04:12:15 -0700

    dev/ changes in makefile

--- frontend/client/nuxt.config.ts
+++ frontend/client/nuxt.config.ts
@@ -5,7 +5,12 @@ export default defineNuxtConfig({
   security: {
     headers: {
       contentSecurityPolicy: {
-        'default-src': ["'self'", "'unsafe-inline'", "data:", "https://maps.googleapis.com"]
+        'default-src': ["'self'", "'unsafe-inline'", "data:", "https://maps.googleapis.com", "https://maps.gstatic.com"],
+        'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://maps.googleapis.com"],
+        'style-src': ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+        'img-src': ["'self'", "data:", "https://*.googleapis.com", "https://*.gstatic.com"],
+        'connect-src': ["'self'", "https://maps.googleapis.com"],
+        'font-src': ["'self'", "https://fonts.googleapis.com", "https://fonts.gstatic.com"]
       }
     }
   },
