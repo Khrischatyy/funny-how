de4f759b155ff9cdea4aea8018d19587bf60ae10
= <rushadaev@gmail.com>
2024-06-21 23:27:42 -0700

    front/meta+manifest

--- frontend/client/nuxt.config.ts
+++ frontend/client/nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1, viewport-fit=cover',
+      viewport: 'width=device-width, initial-scale=1, viewport-fit=cover, maximum-scale=1',
       title: 'Funny How - Empowering Artists Everywhere',
       meta: [
         { name: 'description', content: 'Book, Record, and Create with Ease. Empowering Artists Everywhere.' },
