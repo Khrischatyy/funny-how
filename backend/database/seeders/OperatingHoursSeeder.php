60a29483f0e98b736d90956eca1d546b591eb4c7
tony <khrischatyy@gmail.com>
2024-06-28 05:04:15 -0700

    dev/ fixed operating hours

--- backend/database/seeders/OperatingHoursSeeder.php
+++ backend/database/seeders/OperatingHoursSeeder.php
@@ -17,49 +17,49 @@ class OperatingHoursSeeder extends Seeder
     public function run()
     {
 
-        $inserts = [
-            [
-                'address_id' => 3,
-                'mode_id' => 1,
-                'day_of_week' => null,
-                'open_time' => '00:00',
-                'close_time' => '24:00'
-            ],
-            [
-                'address_id' => 2,
-                'mode_id' => 1,
-                'day_of_week' => null,
-                'open_time' => '00:00',
-                'close_time' => '24:00'
-            ],
-            [
-                'address_id' => 1,
-                'mode_id' => 3,
-                'day_of_week' => 0,
-                'open_time' => '11:00',
-                'close_time' => '17:00'
-            ],
-            [
-                'address_id' => 1,
-                'mode_id' => 4,
-                'day_of_week' => 6,
-                'open_time' => '11:00',
-                'close_time' => '17:00'
-            ],
-        ];
+//        $inserts = [
+//            [
+//                'address_id' => 3,
+//                'mode_id' => 1,
+//                'day_of_week' => null,
+//                'open_time' => '00:00',
+//                'close_time' => '24:00'
+//            ],
+//            [
+//                'address_id' => 2,
+//                'mode_id' => 1,
+//                'day_of_week' => null,
+//                'open_time' => '00:00',
+//                'close_time' => '24:00'
+//            ],
+//            [
+//                'address_id' => 1,
+//                'mode_id' => 3,
+//                'day_of_week' => 0,
+//                'open_time' => '11:00',
+//                'close_time' => '17:00'
+//            ],
+//            [
+//                'address_id' => 1,
+//                'mode_id' => 4,
+//                'day_of_week' => 6,
+//                'open_time' => '11:00',
+//                'close_time' => '17:00'
+//            ],
+//        ];
+//
+//        for ($day = 1; $day <= 5; $day++) {
+//            $inserts[] = [
+//                'address_id' => 1,
+//                'mode_id' => 3,
+//                'day_of_week' => $day,
+//                'open_time' => '10:00',
+//                'close_time' => '18:00'
+//            ];
+//        }
 
-        for ($day = 1; $day <= 5; $day++) {
-            $inserts[] = [
-                'address_id' => 1,
-                'mode_id' => 3,
-                'day_of_week' => $day,
-                'open_time' => '10:00',
-                'close_time' => '18:00'
-            ];
-        }
+//        $isHoursAdded = OperatingHour::insert($inserts);
 
-        $isHoursAdded = OperatingHour::insert($inserts);
-
-        DB::statement("SELECT setval(pg_get_serial_sequence('operating_hours', 'id'), coalesce(max(id)+1, 1), false) FROM operating_hours");
+//        DB::statement("SELECT setval(pg_get_serial_sequence('operating_hours', 'id'), coalesce(max(id)+1, 1), false) FROM operating_hours");
     }
 }
