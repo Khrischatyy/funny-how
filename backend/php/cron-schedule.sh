601698eca629c08d51bcc0f68dcc9bc6e58895df
tony <khrischatyy@gmail.com>
2024-07-25 19:31:29 -0700

    dev/ made expired booking

--- backend/php/cron-schedule.sh
+++ backend/php/cron-schedule.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Add your cron job here
-echo "* * * * * /path/to/your/script.sh" > /etc/crontabs/root
+#echo "* * * * * /path/to/your/script.sh" > /etc/crontabs/root
 
 # Start cron in the foreground (for Docker)
 crond -f
