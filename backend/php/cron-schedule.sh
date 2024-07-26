f7359125f3a5067a7acc017b770d5e01bcbd3161
tony <khrischatyy@gmail.com>
2024-07-25 22:00:39 -0700

    dev/ made cron

--- backend/php/cron-schedule.sh
+++ backend/php/cron-schedule.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 # Add your cron job here
-#echo "* * * * * /path/to/your/script.sh" > /etc/crontabs/root
+echo "* * * * * /usr/local/bin/php /app/artisan schedule:run >> /dev/null 2>&1" > /etc/cron.d/laravel-schedule
+
+# Give execute permission to the cron job file
+chmod 0644 /etc/cron.d/laravel-schedule
+
+# Apply the cron job
+crontab /etc/cron.d/laravel-schedule
 
 # Start cron in the foreground (for Docker)
-#crond -f
+crond -f
